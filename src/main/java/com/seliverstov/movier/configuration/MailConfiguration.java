b67ff00c434fb8a01cab835b1802ca3561a6e2e8
Seliverstov Vitaly <seliverstov_vitaly@mail.ru>
2020-01-21 17:32:34 +0400

    autorization

--- src/main/java/com/seliverstov/movier/configuration/MailConfiguration.java
+++ src/main/java/com/seliverstov/movier/configuration/MailConfiguration.java
@@ -41,8 +41,7 @@ public class MailConfiguration {
 
         properties.setProperty("mail.transport.protocol", protocol);
         properties.setProperty("mail.debug", debug);
-
-
+        
         return mailSender;
     }
 }
