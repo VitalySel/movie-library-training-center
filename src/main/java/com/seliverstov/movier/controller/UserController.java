81efb668fe6ac89176416b7239556e67259c4aac
Seliverstov Vitaly <seliverstov_vitaly@mail.ru>
2020-01-22 22:03:54 +0400

    Add Avatar User fix

--- src/main/java/com/seliverstov/movier/controller/UserController.java
+++ src/main/java/com/seliverstov/movier/controller/UserController.java
@@ -11,6 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.mail.Multipart;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class UserController {
@@ -50,7 +56,23 @@ public class UserController {
         return "addAvatar";
     }
     @PostMapping(value = "/addAvatar")
-    public String addAvatarUser() {
+    public String addAvatarUser(@RequestParam ("file") MultipartFile file, @AuthenticationPrincipal User user) throws IOException {
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename =uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            User usr = user;
+            usr.setAvatar(resultFilename);
+            userService.update(usr);
+        }
         return "redirect:/profile";
     }
 }
