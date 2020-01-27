d99705a06bdfab69cd0a66493e4dfab5d8b111ca
Seliverstov Vitaly <seliverstov_vitaly@mail.ru>
2020-01-27 21:37:51 +0400

    info

---
+++ src/main/java/com/seliverstov/movier/configuration/HibernateConfig.java
@@ -0,0 +1,32 @@
+package com.seliverstov.movier.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class HibernateConfig {
+
+    @Configuration
+    public class MainConfig {
+
+        @Bean
+        public DataSource dataSource() throws URISyntaxException {
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+            DriverManagerDataSource basicDataSource = new DriverManagerDataSource();
+            basicDataSource.setUrl(dbUrl);
+            basicDataSource.setUsername(username);
+            basicDataSource.setPassword(password);
+
+            return basicDataSource;
+        }
+    }
+}
